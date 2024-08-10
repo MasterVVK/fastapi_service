8b9e4f4bc62a4dd027df4aa97579193a008423f8
MasterVVK <vvk@2140101.ru>
2024-08-10 18:32:14 +0500

    Update sync_repo.sh

--- sync_repo.sh
+++ sync_repo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOG_FILE="/srv/fastapi_service/sync_repo.log"
+
 # Чтение конфигурации из config.json
 CONFIG_PATH="$(dirname "$0")/config.json"
 
@@ -8,18 +10,25 @@ ROOT_DIRECTORY=$(grep -oP '(?<="root_directory": ")[^"]*' "$CONFIG_PATH")
 REPO_URL=$(grep -oP '(?<="repo_url": ")[^"]*' "$CONFIG_PATH")
 BRANCH=$(grep -oP '(?<="branch": ")[^"]*' "$CONFIG_PATH")
 
+echo "Starting sync_repo.sh script" >> "$LOG_FILE"
+
 # Переходим в родительскую директорию проекта
 cd "$(dirname "$ROOT_DIRECTORY")" || exit
 
 # Проверка наличия директории проекта
 if [ ! -d "$(basename "$ROOT_DIRECTORY")" ]; then
     # Клонирование репозитория, если директория не существует
-    git clone -b "$BRANCH" "$REPO_URL" "$(basename "$ROOT_DIRECTORY")"
+    echo "Cloning repository" >> "$LOG_FILE"
+    git clone -b "$BRANCH" "$REPO_URL" "$(basename "$ROOT_DIRECTORY")" >> "$LOG_FILE" 2>&1
 else
     # Переходим в директорию проекта
     cd "$ROOT_DIRECTORY" || exit
     # Сброс локальных изменений
-    git reset --hard
+    echo "Resetting local changes" >> "$LOG_FILE"
+    git reset --hard >> "$LOG_FILE" 2>&1
     # Обновление локального репозитория
-    git pull origin "$BRANCH"
+    echo "Pulling latest changes" >> "$LOG_FILE"
+    git pull origin "$BRANCH" >> "$LOG_FILE" 2>&1
 fi
+
+echo "sync_repo.sh script completed" >> "$LOG_FILE"
