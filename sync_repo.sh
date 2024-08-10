798b7cadfb96628ab506c2dd5d403b8c97fe126f
MasterVVK <vvk@2140101.ru>
2024-08-10 16:53:00 +0500

    Update sync_repo.sh

--- sync_repo.sh
+++ sync_repo.sh
@@ -6,6 +6,7 @@ CONFIG_PATH="$(dirname "$0")/config.json"
 # Использование команд Bash для извлечения значений из JSON
 ROOT_DIRECTORY=$(grep -oP '(?<="root_directory": ")[^"]*' "$CONFIG_PATH")
 REPO_URL=$(grep -oP '(?<="repo_url": ")[^"]*' "$CONFIG_PATH")
+BRANCH=$(grep -oP '(?<="branch": ")[^"]*' "$CONFIG_PATH")
 
 # Переходим в родительскую директорию проекта
 cd "$(dirname "$ROOT_DIRECTORY")" || exit
@@ -13,14 +14,12 @@ cd "$(dirname "$ROOT_DIRECTORY")" || exit
 # Проверка наличия директории проекта
 if [ ! -d "$(basename "$ROOT_DIRECTORY")" ]; then
     # Клонирование репозитория, если директория не существует
-    git clone "$REPO_URL" "$(basename "$ROOT_DIRECTORY")"
+    git clone -b "$BRANCH" "$REPO_URL" "$(basename "$ROOT_DIRECTORY")"
+else
+    # Переходим в директорию проекта
+    cd "$ROOT_DIRECTORY" || exit
+    # Сброс локальных изменений
+    git reset --hard
+    # Обновление локального репозитория
+    git pull origin "$BRANCH"
 fi
-
-# Переходим в директорию проекта
-cd "$ROOT_DIRECTORY" || exit
-
-# Сброс локальных изменений
-git reset --hard
-
-# Обновление локального репозитория
-git pull origin main
