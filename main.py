b621d10397a401655d81a6b071808e130bab77ff
MasterVVK <vvk@2140101.ru>
2024-08-10 18:24:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ async def github_webhook(request: Request):
     # Обработка Webhook события push
     event = request.headers.get('X-GitHub-Event')
     if event == "push" and json.loads(payload).get('ref') == 'refs/heads/main':
-        subprocess.run(["sh", "./sync_repo.sh"])
+        subprocess.run(["/usr/bin/sh", "./sync_repo.sh"])
 
     return {"status": "success"}
 
